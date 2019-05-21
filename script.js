f26bb19af42bede34ba8d622f265238b108cf9c9
Glitch (hello-webpage) <none>
2019-05-21 16:14:08 +0000

    🎻😀 Checkpoint ./index.html:378534/148 ./style.css:378534/374 ./script.js:378534/593

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const pressure = document.querySelector('#pressure span')
+const cells = document.querySelector('#cells span')
+const bacteria = document.querySelector('#bacteria span')
+const virus = document.querySelector('#virus span')
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+pressure.textContent = 0.0
+cells.textContent = 0.0
+bacteria.textContent = 0.0
+virus.textContent = 0.0
