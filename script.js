daf7904905f16d8c02928290e0971f2c5ec3feee
Glitch (hello-webpage) <none>
2019-05-21 16:35:08 +0000

    ☔️✈️ Checkpoint ./script.js:378534/765

--- script.js
+++ script.js
@@ -15,15 +15,25 @@ const CELL_MAX = 1.00
 const BACTERIA_LOW = 0.2
 const BACTERIA_HIGH = 0.6
 const VIRUS_LOW = 0.1
-const VIRUS_HIGH = 0.4
+const VIRUS_HIGH = 0.5
 
 function render() {
   pressureVal = (cellsVal / pressureVal) * pressureVal
-  cellsVal = cellsVal - (cellsVal / (bacteriaVal * virusVal))
+  let cellsRandom = Math.random() * cellsRandom
+  cellsVal = cellsVal + (cellsRandom * (bacteriaVal / virusVal))
   
   const bacteriaRandom = Math.random()
-  if (bacteriaRandom > BACTERIA_LOW) {
+  if (bacteriaRandom > BACTERIA_LOW && bacteriaRandom < BACTERIA_HIGH) {
+    bacteriaVal = bacteriaVal + (bacteriaVal / (1 / bacteriaRandom * 100))
+  } else if (bacteriaRandom >= BACTERIA_HIGH)  {
+    bacteriaVal = bacteriaVal + (bacteriaVal / (1 / bacteriaRandom * 50))
+  }
+  
+  const virusRandom = Math.random()
+  if (bacteriaRandom >= BACTERIA_HIGH) {
     
+  } else {
+    virusVal = virusVal + (virusVal / (bacteriaRandom * cellsVal))
   }
   
   pressure.textContent = pressureVal
