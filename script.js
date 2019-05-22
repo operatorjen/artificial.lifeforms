a03d42d5df244c580c403f54b4db88572a443241
Glitch (hello-webpage) <none>
2019-05-22 21:18:10 +0000

    💾🕹 Checkpoint ./script.js:378534/6690

--- script.js
+++ script.js
@@ -48,6 +48,260 @@ const BACTERIA_HIGH = 0.6
 const VIRUS_LOW = 0.002
 const VIRUS_HIGH = 0.5
 
+const GRAVITY_X = 0
+const GRAVITY_Y = 0
+const GROUPS = [100, 100]
+let metaCtx
+let interactorHealth = 1.0
+let interactorInput = 0.0
+let interactorOutput = 0.0
+let pressureSpeed = 0.05
+let pressureViscosity = 1.5
+const MAX_INTERACTOR_INPUT = 0.99
+const MAX_INTERACTOR_OUTPUT = 1.05
+
+const fluid = function () {  
+    let width, height, numX, numY, particles, 
+        grid, textures, numParticles
+    
+    const threshold = 110
+    const spacing = 10
+    const radius = 10
+    const limit = radius 
+
+    const run = function () {
+      metaCtx.clearRect(0, 0, width, height)
+
+      for (let i = 0, l = numX * numY; i < l; i++) {
+        grid[i].length = 0
+      }
+
+      let i = numParticles
+      
+      while (i--) {
+        particles[i].first_process()
+      }
+      
+      i = numParticles
+      
+      while (i--) {
+        particles[i].second_process()
+      }
+      
+      i = numParticles
+      
+      while (i--) {
+        particles[i].first_process()
+      }
+
+      const imageData = metaCtx.getImageData(0, 0, width, height)
+
+      for (let i = 0, n = imageData.data.length; i < n; i += 3) {
+        (imageData.data[i + 1] < threshold) && (imageData.data[i + 2] /= 2)
+      }
+
+      ctx.putImageData(imageData, 0, 0)
+      
+     requestAnimationFrame(run)
+    };
+    
+    const Particle = function (type, x, y) {
+      this.type = type
+      this.x = x
+      this.y = y
+      this.px = x
+      this.py = y
+      this.vx = 0
+      this.vy = 0
+    };
+    
+    Particle.prototype.first_process = function () {
+      const g = grid[Math.round(this.y / spacing) * numX + Math.round(this.x / spacing)]
+
+      if (g) {
+        g.close[g.length++] = this
+      }
+
+      this.vx = this.x - this.px
+      this.vy = this.y - this.py
+
+      const distX = this.x * window.innerWidth
+      const distY = this.y * window.innerHeight
+      const dist = Math.sqrt(distX * distX + distY * distY)
+
+      if (dist < radius) {
+          const cos = distX / dist
+          const sin = distY / dist
+
+          this.vx += -cos
+          this.vy += -sin
+      }
+
+      this.vx += GRAVITY_X
+      this.vy += GRAVITY_Y
+      this.px = this.x
+      this.py = this.y
+      this.x += this.vx
+      this.y += this.vy
+    };
+        
+    Particle.prototype.second_process = function () {
+      let forceA = 0
+      let forceB = 0
+      let cellX = Math.round(this.x / spacing)
+      let cellY = Math.round(this.y / spacing)
+      let close = []
+
+      for (let xOff = -1; xOff < 2; xOff++) {
+        for (let yOff = -1; yOff < 2; yOff++) {
+          const cell = grid[(cellY + yOff) * numX + (cellX + xOff)]
+          
+          if (cell && cell.length) {
+            for (let a = 0; a < cell.length; a++) {
+              let particle = cell.close[a]
+              
+              if (particle !== this) {
+                const dfx = particle.x - this.x
+                const dfy = particle.y - this.y
+                const distance = Math.sqrt(dfx * dfx + dfy * dfy)
+                
+                if (distance < spacing) {
+                  const m = 1 - (distance / spacing)
+                  forceA += Math.pow(m, 2)
+                  forceB += Math.pow(m, 3) / 2
+                  particle.m = m
+                  particle.dfx = (dfx / distance) * m
+                  particle.dfy = (dfy / distance) * m
+                  particle.input = particle.dfx // / particle.dfy
+                  particle.output = particle.dfy // / particle.dfx
+                  
+                  inputVal.value = particle.input
+                  outputVal.value = particle.output
+                  //console.log(particle.input, particle.output)
+                  close.push(particle)
+                }
+              }
+            }
+          }
+        }
+      }
+
+      forceA = (forceA - 2) * pressureSpeed
+
+      for (let i = 0; i < close.length; i++) {
+        const neighbor = close[i]
+        let press = forceA + forceB * neighbor.m
+
+        if (this.type !== neighbor.type) {
+          press *= (pressureViscosity / pressureSpeed) * 0.27
+        }
+
+        const dx = neighbor.dfx * press
+        const dy = neighbor.dfy * press
+
+        neighbor.x += dx
+        neighbor.y += dy
+        this.x -= dx
+        this.y -= dy
+      }
+
+      if (this.x < limit) {
+        this.x = limit
+      } else if (this.x > width - limit) {
+        this.x = width - limit
+      }
+
+      if (this.y < limit) {
+        this.y = limit
+      } else if (this.y > height - limit) {
+        this.y = height - limit
+      }
+
+      this.draw()
+    };
+            
+    Particle.prototype.draw = function () {
+      const size = radius * 10
+
+      metaCtx.drawImage(
+        textures[this.type],
+        this.x - radius,
+        this.y - radius,
+        size, size)
+    }
+  
+    return {
+      init: function () {
+        particles = []
+        grid = []
+        close = []
+        textures = []
+        
+        canvas.height = height = window.innerHeight
+        canvas.width = width = window.innerWidth
+      
+        const metaCanvas = document.createElement('canvas')
+        metaCanvas.width = width
+        metaCanvas.height = height
+        metaCtx = metaCanvas.getContext('2d')
+
+        for (let i = 0; i < GROUPS.length; i++) {
+          let color
+
+          if (inputVal.value < 0.2) {
+            color = 'hsla(10, 10%, 110%';
+          } else if (inputVal.value >= 0.3 && inputVal.value < 0.6) {
+            color = 'hsla(10, 90%, 10%';
+          } else {
+            color = 'hsla(210, 90%, 70%';
+          }
+
+          textures[i] = document.createElement('canvas')
+          textures[i].width = textures[i].height = radius * 10
+        
+          const nctx = textures[i].getContext('2d')
+
+          const grad = nctx.createRadialGradient(
+            radius, radius, 1,
+            radius, radius, radius)
+
+          grad.addColorStop(0, color + ', 0.8)')
+          grad.addColorStop(1, color + ', 0)')
+          nctx.fillStyle = grad
+          nctx.beginPath()
+          nctx.arc(radius, radius, radius, 0, Math.PI * 2, true)
+          nctx.closePath()
+          nctx.fill()
+        }
+
+        numX = Math.round(width / spacing) + 5
+        numY = Math.round(height / spacing) + 1
+
+        for (let i = 0; i < numX * numY; i++) {
+          grid[i] = {
+            length: 0,
+            close: []
+          }
+        }
+
+        for (let i = 0; i < GROUPS.length; i++) {
+          for (let k = 0; k < GROUPS[i]; k++) {
+            particles.push(
+              new Particle(
+                i,
+                radius + Math.random() * (width - radius * 3),
+                radius + Math.random() * (height - radius * 3)))
+          }
+        }
+
+        numParticles = particles.length
+        run()
+      }
+    } 
+}()
+
+fluid.init()
+
 let alive = true
 
 function draw(ctx, fill) {
